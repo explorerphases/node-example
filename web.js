44498819b3d33894181e9d1d56cd9689fab56c7d
Jaycee Vincent Legaspi <explorerphases@gmail.com>
2014-07-19 09:02:13 +0800

    Support for receive and notify
    Added support for receive and notify for Chikka SMS API

--- web.js
+++ web.js
@@ -1,15 +1,27 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var querystring = require("querystring");
+
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
+
+app.get('/smsapi/receive', function(req, res) {
+	console.log(querystring.parse(req));
+	res.send('Accepted');
+});
+
+app.get('/smsapi/notify', function(req, res) {
+	console.log(querystring.parse(req));
+	res.send('Accepted');
+})
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
